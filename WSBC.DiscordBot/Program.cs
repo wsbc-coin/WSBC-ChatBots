e366b6c6fceb5dbbf9863e549e10abf601a0177a
TehGM <wojop95@gmail.com>
2021-02-16 07:46:32 +0100

    Add project files.

---
+++ WSBC.DiscordBot/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace WSBC.Discord
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // build configuration
+            IConfiguration config = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile("appsecrets.json", optional: true)
+                .AddCommandLine(args)
+                .Build();
+        }
+    }
+}
