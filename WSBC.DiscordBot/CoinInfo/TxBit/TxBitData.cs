6b8aac05c8f512caef20edfce7f7b4087f247b5d
TehGM <wojop95@gmail.com>
2021-02-21 12:47:24 +0100

    Update TxBit DTO to be properly readonly

--- WSBC.DiscordBot/CoinInfo/TxBit/TxBitData.cs
+++ WSBC.DiscordBot/CoinInfo/TxBit/TxBitData.cs
@@ -8,43 +8,43 @@ namespace WSBC.DiscordBot.TxBit
     class TxBitData
     {
         [JsonProperty("CurrencyLong")]
-        public string CurrencyName { get; private set; }
+        public string CurrencyName { get; init; }
         [JsonProperty("Currency")]
-        public string CurrencyCode { get; private set; }
+        public string CurrencyCode { get; init; }
         [JsonProperty("AssetType")]
-        public TxBitAssetType AssetType { get; private set; }
+        public TxBitAssetType AssetType { get; init; }
         [JsonProperty("ConsensusType")]
-        public string ConsensusType { get; private set; }
+        public string ConsensusType { get; init; }
         [JsonProperty("Supply")]
-        public decimal Supply { get; private set; }
+        public decimal Supply { get; init; }
         [JsonProperty("MarketCap")]
-        public decimal MarketCap { get; private set; }
+        public decimal MarketCap { get; init; }
         [JsonProperty("BidPrice")]
-        public decimal BidPrice { get; private set; }
+        public decimal BidPrice { get; init; }
         [JsonProperty("Algorithm")]
-        public string Algorithm { get; private set; }
+        public string Algorithm { get; init; }
         [JsonProperty("Website")]
-        public string WebsiteURL { get; private set; }
+        public string WebsiteURL { get; init; }
         [JsonProperty("Explorer")]
-        public string ExplorerURL { get; private set; }
+        public string ExplorerURL { get; init; }
         [JsonProperty("SourceCode")]
-        public string SourceCodeURL { get; private set; }
+        public string SourceCodeURL { get; init; }
         [JsonProperty("Announcement")]
-        public string BitcoinTalkAnnouncementURL { get; private set; }
+        public string BitcoinTalkAnnouncementURL { get; init; }
         [JsonProperty("CoinMarketCap")]
-        public string CoinMarketCapURL { get; private set; }
+        public string CoinMarketCapURL { get; init; }
         [JsonProperty("CoinGecko")]
-        public string CoinGeckoURL { get; private set; }
+        public string CoinGeckoURL { get; init; }
         [JsonProperty("Description")]
-        public string Description { get; private set; }
+        public string Description { get; init; }
         [JsonProperty("BlockCount")]
-        public int BlockCount { get; private set; }
+        public int BlockCount { get; init; }
         [JsonProperty("IsAvailable")]
-        public bool IsAvailable { get; private set; }
+        public bool IsAvailable { get; init; }
         [JsonProperty("LastBlock")]
-        public DateTimeOffset? LastBlockTime { get; private set; }
+        public DateTimeOffset? LastBlockTime { get; init; }
         [JsonProperty("MasternodeCount")]
-        public int MasterNodeCount { get; private set; }
+        public int MasterNodeCount { get; init; }
 
         [JsonConstructor]
         private TxBitData() { }
