496283ffe08d510230d26d32007377e202a160e4
TehGM <wojop95@gmail.com>
2021-04-23 16:10:08 +0200

    Add telegram bot project

---
+++ WSBC.ChatBots.Telegram/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using WSBC.ChatBots.Utilities;
+
+namespace WSBC.ChatBots.Telegram
+{
+    class Program
+    {
+        private static IServiceProvider _services;
+
+        static async Task Main(string[] args)
+        {
+            // handle exiting
+            AppDomain.CurrentDomain.ProcessExit += (sender, e) => OnExit();
+
+            // build configuration
+            IConfiguration config = ConfigurationLoader.BuildDefault(args);
+
+            // enable logging
+            Logging.ConfigureLogging(config);
+
+            try
+            {
+                // prepare DI container
+                IServiceCollection serviceCollection = ConfigureServices(config);
+                _services = serviceCollection.BuildServiceProvider();
+
+                // TODO: start Telegram.Bot client and commands handler
+
+                // wait forever to prevent window closing
+                await Task.Delay(-1).ConfigureAwait(false);
+            }
+            finally
+            {
+                OnExit();
+            }
+        }
+
+        private static IServiceCollection ConfigureServices(IConfiguration configuration)
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            // Logging
+            services.AddSerilogLogging();
+
+            // TODO: Telegram.Bot
+
+            // Memes feature
+            services.AddMemes(configuration: configuration.GetSection("Memes"));
+
+            return services;
+        }
+
+        private static void OnExit()
+        {
+            try { Log.CloseAndFlush(); } catch { }
+            try
+            {
+                if (_services is IDisposable disposableServices)
+                    disposableServices.Dispose();
+            }
+            catch { }
+        }
+    }
+}
