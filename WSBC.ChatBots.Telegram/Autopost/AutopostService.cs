f434305f3ec10b9d19b043a07fdee282fd810ccf
TehGM <wojop95@gmail.com>
2021-04-27 14:14:51 +0200

    Removed redundant namespace

--- WSBC.ChatBots.Telegram/Autopost/AutopostService.cs
+++ WSBC.ChatBots.Telegram/Autopost/AutopostService.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Web;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Telegram.Bot.Args;
